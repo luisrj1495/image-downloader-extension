63a1329998c93bf6342ccf241f07becb59adf7b4
Luis Ernesto Ruiz Jaramillo <luisrj1495@gmail.com>
2021-05-02 00:35:21 -0500

    added: zip build

--- config/plugins/prod.js
+++ config/plugins/prod.js
@@ -1,3 +1,21 @@
+const paths = require("../paths");
+const archiver = require("archiver");
+const fs = require("fs");
+
 module.exports = () => {
-  return [];
+  return [
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap("AfterEmitPlugin", () => {
+          const archive = archiver("zip", {
+            zlib: { level: 9 },
+          });
+          const output = compiler.options.output.path;
+          archive.directory(output, false);
+          archive.pipe(fs.createWriteStream(`${output}.zip`));
+          archive.finalize();
+        });
+      },
+    },
+  ];
 };
